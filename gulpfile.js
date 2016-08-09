c5f1f5bc16702d79caf9c429c7cdcc9be9b3fd9f
Grace Poetzinger <gracepoetzinger@gmail.com>
2016-08-09 14:07:40 -0400

    heavily reworked gulp, changed project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,90 @@
 var gulp = require('gulp'),  
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass'), 
+    postcss = require('gulp-postcss'), //handles css plugins like autoprefixer and cssnano 
+    autoprefixer = require('autoprefixer'), //autoprefixes css
+    cssnano = require('cssnano'), //minifies css
+	sourcemaps = require('gulp-sourcemaps'), //maps css to sass file in the DOM
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    gutil = require('gulp-util'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssnano = require('cssnano'),
-    jade = require('gulp-jade');
-
-var sassMain = ['development/sass/styles.scss'];   
-var jsSources = ['development/js/*.js']; 
+    jade = require('gulp-jade'),
+	imagemin = require('gulp-imagemin'); //optimize images
+	cache = require('gulp-cache') //caches images so that minified images dont get reprocessed
 
 
 
+// SASS
 
-gulp.task('jade', function() {
-  return gulp.src(['development/jade/**/*.jade', '!development/jade/index.jade'])
-    .pipe(jade())
-    .pipe(gulp.dest('reviews'));
-});
-
-gulp.task('jadeIndex', function() {
-  return gulp.src('development/jade/index.jade')
-    .pipe(jade())
-    .pipe(gulp.dest(''));
-});
-
-gulp.task('concat', function() {  
-    gulp.src(jsSources) 
-        .pipe(concat('script.js')) 
-        .pipe(uglify()) 
-        .pipe(gulp.dest('assets/js')); 
-});
-
-gulp.task('watch', function() {  
-//    gulp.watch(sassMain,['sass']); 
-    gulp.watch('development/**/*.scss', ['css']);
-    gulp.watch(jsSources,['concat']); 
-    gulp.watch('development/jade/index.jade', ['jadeIndex']);
-    gulp.watch('development/jade/**/*.jade', ['jade']);
-});
+var sassSrc = ['app/src/sass/*.scss'],
+	sassDist = ['app/dist/css'];
 
 gulp.task('css', function () {
     var processors = [
         autoprefixer(),
-        cssnano(),
-    ];
-    return gulp.src('development/sass/styles.scss')
-        .pipe(sass().on('error', sass.logError))
+        cssnano(), 
+    ],
+	sassOptions = {
+	  errLogToConsole: true,
+	  outputStyle: 'compressed'
+	};
+    return gulp.src(sassSrc)
+		.pipe(sourcemaps.init())
+        .pipe(sass(sassOptions))
         .pipe(postcss(processors))
-        .pipe(gulp.dest('assets/css'));
+		.pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('app/dist/css'));
 });
 
-gulp.task('default', ['jade', 'jadeIndex', 'css', 'concat', 'watch']);  
+// JAVASCRIPT
+
+var jsSrc = ['app/src/js/*.js'],
+	jsDist = ['app/dist/js'];
+
+gulp.task('js', function() {  
+    gulp.src(jsSrc) 
+        .pipe(concat('script.js')) 
+        .pipe(uglify()) 
+        .pipe(gulp.dest('app/dist/js')); 
+});
+
+// JADE
+
+var jadeSrc = ['app/src/jade/**/*.js'],
+    jadeIndexSrc = ['app/src/jade/*.jade'],
+    jadeReviewsSrc = ['app/src/jade/reviews/*.jade'];
+
+gulp.task('jadeIndex', function() {
+  return gulp.src(jadeIndexSrc)
+    .pipe(jade())
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('jadeReviews', function() {
+  return gulp.src(jadeReviewsSrc)
+    .pipe(jade())
+    .pipe(gulp.dest('reviews'));
+});
+
+gulp.task('jade', ['jadeIndex', 'jadeReviews']);
+
+// ASSETS
+
+gulp.task('images', function(){
+  return gulp.src('app/src/img/**/*.+(png|jpg|jpeg|gif|svg)')
+//  .pipe(cache(imagemin({
+//      interlaced: true
+//    })))
+  .pipe(imagemin())
+  .pipe(gulp.dest('app/dist/img'))
+});
+
+// WATCH
+
+gulp.task('watch', function() {  
+    gulp.watch(sassSrc, ['css']);
+    gulp.watch(jsSrc, ['js']); 
+	gulp.watch([jadeIndexSrc, jadeReviewsSrc], ['jade']);
+});
+
+
+gulp.task('default', ['css', 'js', 'jade', 'images', 'watch']);  
 
